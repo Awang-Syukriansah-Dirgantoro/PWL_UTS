16f076e5d73773b8ea08fcf46ebe071a1ef0089f
Awang-Syukriansah-Dirgantoro <awcvk130201@gmail.com>
2021-03-30 14:43:17 +0700

    menambah controller dan edit web.php

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Menu;
+use Illuminate\Support\Facades\DB;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $menu = Menu::all();
+        $list = Menu::orderBy('kode_barang','desc')->paginate(5);
+        return view('main.index',compact('menu','list'))->with('i',(request()->input('page',1)-1)*5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('main.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'kategori_barang' => 'required',
+            'harga' => 'required',
+            'qty' => 'required',
+        ]);
+        Menu::create($request->all());
+        return redirect()->route('main.index')->with('success','Menu berhasil ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $menu = Menu::find($id);
+        return view('main.detail',compact('menu'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $menu = Menu::find($id);
+        return view('main.edit',compact('menu'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'kode_barang' => 'required',
+            'nama_barang' => 'required',
+            'kategori_barang' => 'required',
+            'harga' => 'required',
+            'qty' => 'required',
+        ]);
+        Menu::find($id)->update($request->all());
+        return redirect()->route('main.index')->with('success','Menu berhasil diupdate');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Menu::find($id)->delete();
+        return redirect()->route('main.index')->with('success','Menu berhasil  dihapus');
+    }
+
+    public function cari(Request $request)
+	{
+		$menu = Menu::where('id',$request->id)->first();
+        return view('main.cari',compact('menu'));
+	}
+}
